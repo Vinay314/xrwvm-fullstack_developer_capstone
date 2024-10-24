888eec59b0b5e4363a9219c152f5fdb5d082313b
Vinay Rupchandani <vinay.rupchandani21@vit.edu>
2024-10-24 06:21:23 -0400

    Reviews and dealers

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" Component={Register} />
       <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" Component={Dealer} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
